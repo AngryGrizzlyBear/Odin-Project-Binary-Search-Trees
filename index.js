562b81133abeb1ed8f318561d76558bb6f297a9a
Evan <24954397+AngryGrizzlyBear@users.noreply.github.com>
2025-10-22 08:43:45 -0700

    Added insert(value) to tree

--- index.js
+++ index.js
@@ -27,17 +27,35 @@ class Tree {
 
         return build(sortedArray);
     }
+    insert(value, node = this.root) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value === node.data) return node;
+        if (value < node.data) {
+            node.left = this.insert(value, node.left);
+        } else {
+            node.right = this.insert(value, node.right);
+        }
+        return node;
+    }
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
         prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
     }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
 
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
 };
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root);
+
+tree.insert(10);
+tree.insert(2);
+tree.insert(5000);
 prettyPrint(tree.root);
